283313af0111e83a3e6d1a63b0a1e2f3e0f9a57e
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2024-01-26 14:32:28 +0900

    Remove comments

--- src/main.rs
+++ src/main.rs
@@ -64,22 +64,6 @@ fn escaped_display(path: &Path) -> String {
     escape_csv(&path.display().to_string())
 }
 
-// fn read_digest<R: Read, H: Digest + FixedOutputReset>(
-//     hasher: &mut H,
-//     mut read: R,
-//     buffer: &mut [u8],
-// ) -> Result<digest::Output<H>> {
-//     loop {
-//         let n = read.read(buffer)?;
-//         if n == 0 {
-//             break;
-//         }
-//         Digest::update(hasher, &buffer[..n]);
-//     }
-//     let hash = hasher.finalize_reset();
-//     Ok(hash)
-// }
-
 struct BufHash<H: Digest + FixedOutputReset> {
     hasher: H,
     hash: digest::Output<H>,
@@ -106,29 +90,7 @@ where
         }
     }
 
-    // fn digest<R: Read>(&self, path: &Path, mut readable: R) -> Result<()> {
-    //     loop {
-    //         let n = readable.read(&mut self.buffer)?;
-    //         if n == 0 {
-    //             break;
-    //         }
-    //         Digest::update(&mut self.hasher, &self.buffer[..n]);
-    //     }
-    //     digest::FixedOutputReset::finalize_into_reset(&mut self.hasher, &mut self.hash);
-    //     match self.format {
-    //         PrintFormat::Sum => {
-    //             println!("{:x}  {}", self.hash, path.display());
-    //         }
-    //         PrintFormat::Csv => {
-    //             println!("{:x},{}", self.hash, escaped_display(path));
-    //         }
-    //     }
-    //     Ok(())
-    // }
-
     fn digest_file(&mut self, path: &Path) -> Result<()> {
-        // let mut file = File::open(path)?;
-        // self.digest(path, file)
         let mut file = File::open(path)?;
         loop {
             let n = file.read(&mut self.buffer)?;
@@ -175,73 +137,7 @@ where
         }
         Ok(())
     }
-    // fn digest_zip_entry(&self, entry:)
 }
-
-// fn stream_digest<H>(
-//     hasher: BufHash<H>,
-//     path: &Path,
-//     buffer_size: usize,
-//     print_format: PrintFormat,
-// ) -> Result<()>
-// where
-//     H: Digest + FixedOutputReset,
-// {
-//     let file = File::open(path)?;
-//     let mut buffer = vec![0; buffer_size];
-//     let hasher = Md5::new();
-//     let hash = read_digest(hasher, file, buffer.as_mut_slice())?;
-//     match print_format {
-//         PrintFormat::Sum => {
-//             println!("{:x}  {}", hash, path.display());
-//         }
-//         PrintFormat::Csv => {
-//             println!("{:x},{}", hash, escaped_display(path));
-//         }
-//     }
-//     Ok(())
-// }
-
-// fn process_zip<H>(
-//     hasher: &BufHash<H>,
-//     path: &Path,
-//     buffer_size: usize,
-//     print_format: PrintFormat,
-// ) -> Result<()>
-// where
-//     H: Digest + FixedOutputReset,
-//     <H as digest::OutputSizeUser>::OutputSize: std::ops::Add,
-//     <<H as digest::OutputSizeUser>::OutputSize as std::ops::Add>::Output:
-//         digest::generic_array::ArrayLength<u8>,
-// {
-//     let file = File::open(path)?;
-//     let mut archive = zip::ZipArchive::new(file)?;
-//     let mut digest = Md5::new();
-//     for i in 0..archive.len() {
-//         let mut file = archive.by_index(i)?;
-//         if file.is_dir() {
-//             continue;
-//         }
-//         let mut buffer = vec![0; buffer_size];
-//         loop {
-//             let n = file.read(&mut buffer)?;
-//             if n == 0 {
-//                 break;
-//             }
-//             digest.update(&buffer[..n]);
-//         }
-//         let hash = digest.finalize_reset();
-//         match print_format {
-//             PrintFormat::Sum => {
-//                 println!("{:x}  {}/{}", hash, path.display(), file.name());
-//             }
-//             PrintFormat::Csv => {
-//                 println!("{:x},{}", hash, escaped_display(path));
-//             }
-//         }
-//     }
-//     Ok(())
-// }
 
 fn process_input<H>(
     hasher: &mut BufHash<H>,
@@ -258,7 +154,6 @@ where
     debug!("process_input: {}", input.display());
     if input.is_file() {
         if archive && input.extension().unwrap_or_default() == "zip" {
-            // process_zip(&hasher, input, buffer_size, print_format)?;
             hasher.digest_zip(input)?;
         } else {
             hasher.digest_file(input)?;
@@ -316,29 +211,4 @@ fn main() -> Result<()> {
         Algorithm::Md5 => execute::<Md5>(args),
         Algorithm::Sha1 => execute::<sha1::Sha1>(args),
     }
-
-    // let hasher: Box<dyn Hasher> = Box::new(BufHash::new(buffer_size));
-    // for input in args.input {
-    //     if input.is_file() {
-    //         process_input(
-    //             &input,
-    //             buffer_size,
-    //             args.format,
-    //             args.recursive,
-    //             args.archive,
-    //         )?;
-    //     } else if input.is_dir() {
-    //         for entry in read_dir(&input)? {
-    //             let entry = entry?;
-    //             process_input(
-    //                 entry.path().as_path(),
-    //                 buffer_size,
-    //                 args.format,
-    //                 args.recursive,
-    //                 args.archive,
-    //             )?;
-    //         }
-    //     };
-    // }
-    // Ok(())
 }
